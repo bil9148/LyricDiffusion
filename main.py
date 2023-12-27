06fea1bc872841fa32e6a8e858b277d4c2555351
Andrej Blazevic <andrejblaza12@gmail.com>
2023-12-27 17:07:29 +0100

    Need to explore more models to get the best results

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ def run():
     try:
         """Runs the program"""
 
-        songName = input("Enter the song name: ")
-        artistName = input("Enter the artist name: ")
+        # songName = input("Enter the song name: ")
+        # artistName = input("Enter the artist name: ")
+
+        songName = "Hello"
+        artistName = "Adele"
 
         assert len(songName) > 0, "Song name cannot be empty"
         assert len(artistName) > 0, "Artist name cannot be empty"
@@ -17,8 +20,15 @@ def run():
 
         assert len(verses) > 0, "No lyrics found"
 
+        # DGSpitzer/Cyberpunk-Anime-Diffusion
+        # stabilityai/sdxl-turbo - Didn't work
+        # SG161222/Realistic_Vision_V6.0_B1_noVAE - Didn't work
+        # SG161222/Realistic_Vision_V2.0 - Didn't work
+        # Lykon/DreamShaper - Didn't work
+        # stabilityai/stable-diffusion-2-1 - Downloaded
+
         l2i = lyrics2Images.Lyrics2Images(
-            num_inference_steps=100, use_auth_token=False, prompt="Vintage art", model_id="CompVis/stable-diffusion-v1-4", revision="fp16")
+            num_inference_steps=50, use_auth_token=False, prompt=f"vibrant and joyful visuals inspired by the sunny lyrics of {songName} by {artistName}", model_id="DGSpitzer/Cyberpunk-Anime-Diffusion", revision="fp16")
 
         outputPath = rf"C:\Users\andre\source\repos\AIG\images\{songName} - {artistName}"
 
