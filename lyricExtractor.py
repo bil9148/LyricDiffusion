10532a03ac3d5db14b758e9449ea474e8beb57df
Andrej Blazevic <andrejblaza12@gmail.com>
2023-12-27 15:48:37 +0100

    Successfully generated Pop Smoke - Dior 🔥🔥🔥

--- lyricExtractor.py
+++ lyricExtractor.py
@@ -4,15 +4,33 @@ from lyricsgenius import Genius
 def getLyrics(songName: str, artistName: str):
     """Returns the lyrics of a song given the song name and artist name"""
 
-    geniusApiKey = "0r7QN8utjRy_k7Ims4oLjaxHzBo9XPbtKi9W8IE6vTdgIhpTus0IZ8TOYHMFJMUQ"
-    genius = Genius(geniusApiKey)
+    try:
+        geniusApiKey = "0r7QN8utjRy_k7Ims4oLjaxHzBo9XPbtKi9W8IE6vTdgIhpTus0IZ8TOYHMFJMUQ"
+        genius = Genius(geniusApiKey)
 
-    song = genius.search_song(songName, artistName)
+        song = genius.search_song(songName, artistName)
 
-    # Split the lyrics into verses
-    verses = song.lyrics.split("\n")
+        # Ask user if the found song is the correct one
+        if song is None:
+            raise Exception("Song not found")
 
-    return verses
+        songCorrect = input(
+            f"Is this the correct song? (y/n)\n{song.artist}-{song.title}\n")
+
+        if songCorrect.lower() != "y":
+            raise Exception("Song not found")
+
+        # Split the lyrics into verses
+        verses = song.lyrics.split("\n")
+
+        if len(verses) == 0:
+            raise Exception("No lyrics found")
+
+        return verses
+
+    except Exception as e:
+        print(e)
+        return
 
 
 def testGetLyrics():
