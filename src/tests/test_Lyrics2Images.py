0b5a7c240cd6950fe7db5e73a38535fba77638d9
Andrej Blazevic <andrejblaza12@gmail.com>
2023-12-28 15:23:41 +0100

    small fix

--- src/tests/test_Lyrics2Images.py
+++ src/tests/test_Lyrics2Images.py
@@ -3,19 +3,21 @@ import unittest
 import tempfile
 import shutil
 import os
+from output import OUTPUT_PATH
 
 
 class TestLyrics2Images(unittest.TestCase):
     def setUp(self):
         self.temp_dir = tempfile.mkdtemp()
 
-    def tearDown(self):
-        shutil.rmtree(self.temp_dir)
+    # def tearDown(self):
+        # shutil.rmtree(self.temp_dir)
 
     def test_runL2I_with_valid_verses(self):
         # Arrange
         verses = ["Verse 1", "Verse 2", "Verse 3"]
-        output_path = os.path.join(self.temp_dir, "output")
+        output_path = os.path.join(
+            OUTPUT_PATH, "images", "test")
 
         # Act
         lyrics2images = Lyrics2Images(use_auth_token=False)
