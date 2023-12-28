f8c60a9feb584a443636d540b6f7ca6f2c2aa75a
Andrej Blazevic <andrejblaza12@gmail.com>
2023-12-28 15:10:09 +0100

    Basic GUI functional

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 from output import configure_logging
 from lyrics2Images import run
-
+from gui import showGUI
 
 if __name__ == "__main__":
     configure_logging()
-    run("Gangstas Paradise", "Coolio",
-        "stabilityai/stable-diffusion-2-1", 50)
+    showGUI()
