8a0ed2b838f312f9771bb6015919bbcb1b43ef48
Andrej Blazevic <andrejblaza12@gmail.com>
2024-01-02 17:02:21 +0100

    db tests implemented

--- src/tests/test_Database.py
+++ src/tests/test_Database.py
@@ -4,24 +4,32 @@ from db import Database
 
 class TestDatabase(unittest.TestCase):
     def setUp(self):
+        self.testing_DB_name = "testing_db42069"
         self.database = Database(
             host="127.0.0.1", port="5432", user="postgres", password="sqlpostgres", database="postgres")
 
         self.database.connect()
 
         # Check if testing_db exists
-        result = self.database.fetch_one("SELECT 1 FROM pg_database WHERE datname = %s", ("testing_db",))
+        result = self.database.fetch_one(
+            f"SELECT 1 FROM pg_database WHERE datname = '{self.testing_DB_name}'")
 
         if not result:
             # Drop testing_db if it exists
-            self.database.execute("DROP DATABASE IF EXISTS testing_db")
+            self.database.execute(
+                f"DROP DATABASE IF EXISTS {self.testing_DB_name}")
             # Create testing_db
-            self.database.execute("CREATE DATABASE testing_db")
+            self.database.execute(f"CREATE DATABASE {self.testing_DB_name}")
 
         # Connect to testing_db
-        self.database.database = "testing_db"
+        self.database.database = self.testing_DB_name
 
     def tearDown(self):
+        # Drop testing_db
+        self.database.database = "postgres"
+
+        self.database.execute(f"DROP DATABASE IF EXISTS {self.testing_DB_name}")
+
         self.database.disconnect()
 
     def test_connection(self):
@@ -46,6 +54,14 @@ class TestDatabase(unittest.TestCase):
     def test_disconnect(self):
         self.database.disconnect()
         self.assertIsNone(self.database.conn, "Connection should be closed")
+
+    def test_setupDatabase(self):
+        # Check if lyrics2images database exists
+        self.database.setupDatabase()
+
+        result = self.database.fetch_one("SELECT 1 FROM pg_database WHERE datname = 'lyrics2images'")
+
+        self.assertIsNotNone(result, "lyrics2images database should exist")
 
 
 if __name__ == "__main__":
