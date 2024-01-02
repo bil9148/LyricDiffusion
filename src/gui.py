8ec9f48a97e4ad959c4d7025c04394e7306cfbe7
Andrej Blazevic <andrejblaza12@gmail.com>
2024-01-02 09:47:08 +0100

    Settings implementation start

--- src/gui.py
+++ src/gui.py
@@ -11,14 +11,70 @@ class FontManager:
     def getFont() -> QtGui.QFont:
         return QtGui.QFont("Arial", 14)
 
+class BasicUI:
+    @staticmethod
+    def HandleError(message, silent=False):
+        logging.error(message)
+        
+        if not silent:
+            MsgBox(message)
+
+    @staticmethod
+    def MsgBox(e):
+        msg = QtWidgets.QMessageBox()
+        msg.setIcon(QtWidgets.QMessageBox.Critical)
+        msg.setText(str(e))
+        msg.setWindowTitle("Lyrics2Images")
+        msg.setStandardButtons(QtWidgets.QMessageBox.Ok)            
+        msg.exec_()
+
+    @staticmethod
+    def create_label(text):
+        label = QtWidgets.QLabel(text)
+        label.setFont(FontManager.getFont())
+        return label
+
+    @staticmethod
+    def create_textbox(read_only=False):
+        textbox = QtWidgets.QLineEdit()
+        textbox.setFont(FontManager.getFont())
+        textbox.setReadOnly(read_only)
+        return textbox
+
+    @staticmethod
+    def create_progress_bar():
+        progress_bar = QtWidgets.QProgressBar()
+        progress_bar.setFont(FontManager.getFont())
+        progress_bar.setMinimum(0)
+        progress_bar.setValue(0)
+        progress_bar.setSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        return progress_bar
+
+    @staticmethod
+    def create_model_list( itemList: Optional[list] = None):
+        model_list = QtWidgets.QComboBox()
+
+        if itemList is not None:
+            model_list.addItems(itemList)
+
+        model_list.setFont(FontManager.getFont())
+        return model_list
+
+    @staticmethod
+    def create_button( text):
+        button = QtWidgets.QPushButton(text)
+        button.setFont(FontManager.getFont())
+        return button
+
 class SettingsWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
         # Create widgets and set common font
-        self.label_outputPath = self.create_label("Output path:")
-        self.textbox_outputPath = self.create_textbox()
-        self.button_browseOutputPath = self.create_button("Browse")
+        self.label_outputPath = BasicUI.create_label("Output path:")
+        self.textbox_outputPath = BasicUI.create_textbox()
+        self.button_browseOutputPath = BasicUI.create_button("Browse")
 
         # Connect button signal
         self.button_browseOutputPath.clicked.connect(self.browseOutputPath)
@@ -34,29 +90,24 @@ class SettingsWidget(QtWidgets.QWidget):
         self.setLayout(self.layout)
 
 
-
-
 class LyricsGeneratorWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
         # Create widgets and set common font
-        self.label_songName = self.create_label("Song name:")
-        self.label_artistName = self.create_label("Artist name:")
-        self.textbox_songName = self.create_textbox()
-        self.textbox_artistName = self.create_textbox()
-        self.label_modelList = self.create_label("Model:")
-        self.label_numInferenceSteps = self.create_label(
+        self.label_songName = BasicUI.create_label("Song name:")
+        self.label_artistName = BasicUI.create_label("Artist name:")
+        self.textbox_songName = BasicUI.create_textbox()
+        self.textbox_artistName = BasicUI.create_textbox()
+        self.label_modelList = BasicUI.create_label("Model:")
+        self.label_numInferenceSteps = BasicUI.create_label(
             "Num inference steps:")
-        self.textbox_numInferenceSteps = self.create_textbox()
+        self.textbox_numInferenceSteps = BasicUI.create_textbox()
         self.textbox_numInferenceSteps.setText("50")
-        self.label_info = self.create_label("Info:")
-        self.textbox_info = self.create_textbox(read_only=True)
-        self.loading_bar = self.create_progress_bar()
-        #Item list:
-        # model_list.addItem("stabilityai/stable-diffusion-2-1")
-        # model_list.addItem("DGSpitzer/Cyberpunk-Anime-Diffusion")
-        # Generates black images
+        self.label_info = BasicUI.create_label("Info:")
+        self.textbox_info = BasicUI.create_textbox(read_only=True)
+        self.loading_bar = BasicUI.create_progress_bar()        
+        # Generate black images
         # model_list.addItem("dataautogpt3/OpenDalleV1.1")
         # model_list.addItem("stabilityai/sdxl-turbo")
         # Don't work - no such modeling files are available
@@ -64,11 +115,13 @@ class LyricsGeneratorWidget(QtWidgets.QWidget):
         # self.modelList.addItem("SG161222/Realistic_Vision_V6.0_B1_noVAE")
         # self.modelList.addItem("Lykon/DreamShaper")
         itemList = ["stabilityai/stable-diffusion-2-1", "DGSpitzer/Cyberpunk-Anime-Diffusion"]
-        self.modelList = self.create_model_list(itemList=itemList)
-        self.button_generate = self.create_button("Generate")
+        self.modelList = BasicUI.create_model_list(itemList=itemList)
+        self.button_generate = BasicUI.create_button("Generate")
+        self.button_settings = BasicUI.create_button("Settings")
 
         # Connect button signal
         self.button_generate.clicked.connect(self.generate)
+        self.button_settings.clicked.connect(self.showSettings)
 
         # Create layout and add widgets
         self.layout = QtWidgets.QGridLayout()
@@ -83,43 +136,14 @@ class LyricsGeneratorWidget(QtWidgets.QWidget):
         self.layout.addWidget(self.label_info, 4, 0)
         self.layout.addWidget(self.textbox_info, 4, 1)
         self.layout.addWidget(self.loading_bar, 5, 0, 1, -1)
-        self.layout.addWidget(self.button_generate, 6, 0, 1, -1)
+        self.layout.addWidget(self.button_generate, 6, 1) 
+        self.layout.addWidget(self.button_settings, 6, 0) 
+
 
         self.setLayout(self.layout)    
 
-    def create_label(self, text):
-        label = QtWidgets.QLabel(text)
-        label.setFont(FontManager.getFont())
-        return label
-
-    def create_textbox(self, read_only=False):
-        textbox = QtWidgets.QLineEdit()
-        textbox.setFont(FontManager.getFont())
-        textbox.setReadOnly(read_only)
-        return textbox
-
-    def create_progress_bar(self):
-        progress_bar = QtWidgets.QProgressBar()
-        progress_bar.setFont(FontManager.getFont())
-        progress_bar.setMinimum(0)
-        progress_bar.setValue(0)
-        progress_bar.setSizePolicy(
-            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
-        return progress_bar
-
-    def create_model_list(self, itemList: Optional[list] = None):
-        model_list = QtWidgets.QComboBox()
-
-        if itemList is not None:
-            model_list.addItems(itemList)
-
-        model_list.setFont(FontManager.getFont())
-        return model_list
-
-    def create_button(self, text):
-        button = QtWidgets.QPushButton(text)
-        button.setFont(FontManager.getFont())
-        return button
+    def showSettings(self):
+        pass
 
     def generate(self):
         try:                
@@ -143,22 +167,8 @@ class LyricsGeneratorWidget(QtWidgets.QWidget):
                             model_id=model_id, num_inference_steps=num_inference_steps, uiWidget=self)
         except Exception as e:
             message = e.args[0] if len(e.args) > 0 else str(e)
-            self.HandleError(message)
-            self.loading_bar.setValue(0)
-
-    def HandleError(self, message, silent=False):
-        logging.error(message)
-        
-        if not silent:
-            self.MsgBox(message)
-
-    def MsgBox(self, e):
-        msg = QtWidgets.QMessageBox()
-        msg.setIcon(QtWidgets.QMessageBox.Critical)
-        msg.setText(str(e))
-        msg.setWindowTitle("Lyrics2Images")
-        msg.setStandardButtons(QtWidgets.QMessageBox.Ok)            
-        msg.exec_()
+            BasicUI.HandleError(message)
+            self.loading_bar.setValue(0)#
 
 class LyricsGeneratorApp(QtWidgets.QMainWindow):
     def __init__(self):
