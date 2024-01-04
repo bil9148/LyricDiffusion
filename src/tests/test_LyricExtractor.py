6a5b354404fb4ddde4483acee3410a4a9668f1d7
Andrej Blazevic <andrejblaza12@gmail.com>
2024-01-04 12:14:38 +0100

    added some tests

--- src/tests/test_LyricExtractor.py
+++ src/tests/test_LyricExtractor.py
@@ -1,4 +1,4 @@
-from ..lyricExtractor import getLyrics
+from ..lyricExtractor import getLyrics, getSong
 import unittest
 from unittest.mock import patch
 
@@ -8,7 +8,7 @@ class TestGetLyrics(unittest.TestCase):
     @patch("lyricExtractor.Genius")
     def test_get_lyrics_successful(self, mock_genius):
         # Call the function with a known song and artist
-        result = getLyrics("Coolio", "Gangsta's Paradise",askIfCorrect=False)
+        result = getLyrics("Coolio", "Gangsta's Paradise", askIfCorrect=False)
 
         # Assertions
         self.assertTrue(len(result) > 0)
@@ -16,10 +16,25 @@ class TestGetLyrics(unittest.TestCase):
     @patch("lyricExtractor.Genius")
     def test_get_lyrics_song_not_found(self, mock_genius):
         # Call the function with a known song and artist
-        result = getLyrics("NonExistentSong", "ExampleArtist",askIfCorrect=False)
+        result = getLyrics("NonExistentSong",
+                           "ExampleArtist", askIfCorrect=False)
 
         # Assertions
         self.assertListEqual(result, [])
+
+    def test_getSong_successful(self):
+        # Call the function with a known song and artist
+        result = getSong("Coolio", "Gangsta's Paradise")
+
+        # Assertions
+        self.assertIsNotNone(result)
+
+    def test_getSong_song_not_found(self):
+        # Call the function with a known song and artist
+        result = getSong("NonExistentSong", "ExampleArtist")
+
+        # Assertions
+        self.assertIsNone(result)
 
 
 if __name__ == "__main__":
