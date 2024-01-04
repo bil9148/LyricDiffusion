b8c68b00d7c164c112404a8f0f29d555f6ff479f
Andrej Blazevic <andrejblaza12@gmail.com>
2024-01-04 11:51:29 +0100

    added tests for settings.py

---
+++ src/tests/test_Settings.py
@@ -0,0 +1,58 @@
+import unittest
+import logging
+from unittest.mock import patch
+from settings import OutputPath, SkipEmptyVerses, configure_logging
+
+
+class TestOutputPath(unittest.TestCase):
+
+    def test_get_default_output_path(self):
+        default_output_path = OutputPath.getDefaultOutputPath()
+        self.assertIsInstance(default_output_path, str)
+
+    def test_set_output_path(self):
+        with patch('logging.info') as mock_logging_info:
+            OutputPath.setOutputPath("/path/to/output")
+            mock_logging_info.assert_called_with(
+                "Output path updated to /path/to/output.")
+
+    def test_get_output_path(self):
+        expected_output_path = "/path/to/output"
+        OutputPath.setOutputPath(expected_output_path)
+        output_path = OutputPath.getOutputPath()
+        self.assertEqual(output_path, expected_output_path,
+                         f"output_path: {output_path}, expected: {expected_output_path}")
+
+
+class TestSkipEmptyVerses(unittest.TestCase):
+
+    def test_set_skip_empty_verses(self):
+        with patch('logging.info') as mock_logging_info:
+            SkipEmptyVerses.setSkipEmptyVerses(True)
+            mock_logging_info.assert_called_with(
+                "Skip empty verses updated to True.")
+
+    def test_get_skip_empty_verses_true(self):
+        expected_skip_empty_verses = True
+        SkipEmptyVerses.setSkipEmptyVerses(expected_skip_empty_verses)
+        result = SkipEmptyVerses.getSkipEmptyVerses()
+        self.assertEqual(result, expected_skip_empty_verses,
+                         f"result: {result}, expected: {expected_skip_empty_verses}")
+
+    def test_get_skip_empty_verses_false(self):
+        expected_skip_empty_verses = False
+        SkipEmptyVerses.setSkipEmptyVerses(expected_skip_empty_verses)
+        result = SkipEmptyVerses.getSkipEmptyVerses()
+        self.assertEqual(result, expected_skip_empty_verses,
+                         f"result: {result}, expected: {expected_skip_empty_verses}")
+
+
+class TestConfigureLogging(unittest.TestCase):
+
+    def test_configure_logging(self):
+        configure_logging()
+        self.assertEqual(logging.getLogger().level, logging.INFO)
+
+
+if __name__ == '__main__':
+    unittest.main()
