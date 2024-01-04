6bc409da8c771d917d01fa7f5ddac59f1ae2e3d4
Andrej Blazevic <andrejblaza12@gmail.com>
2024-01-04 11:37:52 +0100

    Not using postgresql anymore to store the 2 settings... Just makes no sense having to install it for something like that. Instead it just changes the variables in ram now

--- src/settings.py
+++ src/settings.py
@@ -1,17 +1,17 @@
 import logging
 import os
-import db
-import gui
+# import db
+
+Output_Path = None
+
 
 class OutputPath:
     @staticmethod
     def getOutputPath():
-        output_Path = db.DATABASE.fetch_one("SELECT value FROM settings where name = 'output_path';")
-
-        if output_Path is None:
-            return OutputPath.getDefaultOutputPath()
-
-        return output_Path[0]
+        global Output_Path
+        if Output_Path is None:
+            Output_Path = OutputPath.getDefaultOutputPath()
+        return Output_Path
 
     @staticmethod
     def getDefaultOutputPath():
@@ -20,45 +20,87 @@ class OutputPath:
         # Remove '/src/' from the path
         output_path = script_directory.replace("src", "")
         return output_path
-    
+
     @staticmethod
     def setOutputPath(output_Path):
-        # First delete the old output path
-        db.DATABASE.execute("DELETE FROM settings WHERE name = 'output_path';")
-        # Then insert the new output path
-        db.DATABASE.execute("INSERT INTO settings (name, value) VALUES ('output_path', %s);", (output_Path,))
-        logging.info(f"Output path updated to {output_Path} in database.")
+        global Output_Path
+        Output_Path = output_Path
+        logging.info(f"Output path updated to {Output_Path}.")
+
+    # @staticmethod
+    # def getOutputPath():
+    #     output_Path = db.DATABASE.fetch_one(
+    #         "SELECT value FROM settings where name = 'output_path';")
+
+    #     if output_Path is None:
+    #         return OutputPath.getDefaultOutputPath()
+
+    #     return output_Path[0]
+
+    # @staticmethod
+    # def setOutputPath(output_Path):
+    #     # First delete the old output path
+    #     db.DATABASE.execute("DELETE FROM settings WHERE name = 'output_path';")
+    #     # Then insert the new output path
+    #     db.DATABASE.execute(
+    #         "INSERT INTO settings (name, value) VALUES ('output_path', %s);", (output_Path,))
+
+    #     logging.info(f"Output path updated to {Output_Path} in database.")
+
+
+SkipVerses = True
+
 
 class SkipEmptyVerses:
+    # @staticmethod
+    # def setSkipEmptyVerses(skipEmptyVerses):
+    #     # # First delete the old output path
+    #     # db.DATABASE.execute(
+    #     #     "DELETE FROM settings WHERE name = 'skip_empty_verses';")
+    #     # # Then insert the new output path
+    #     # db.DATABASE.execute(
+    #     #     "INSERT INTO settings (name, value) VALUES ('skip_empty_verses', %s);", (skipEmptyVerses,))
+    #     logging.info(
+    #         f"Skip empty verses updated to {skipEmptyVerses} in database.")
+
+    # @staticmethod
+    # def getSkipEmptyVerses():
+    # try:
+    #     skipEmptyVerses = db.DATABASE.fetch_one(
+    #         "SELECT value FROM settings where name = 'skip_empty_verses';")
+
+    #     if skipEmptyVerses is None:
+    #         return True
+
+    #     # Parse the result
+    #     skipEmptyVerses = skipEmptyVerses[0]
+
+    #     skipEmptyVerses = skipEmptyVerses.capitalize()
+
+    #     if skipEmptyVerses == "True":
+    #         return True
+    #     elif skipEmptyVerses == "False":
+    #         return False
+    #     else:
+    #         raise Exception(
+    #             f"Invalid value for skip_empty_verses: {skipEmptyVerses}")
+    # except Exception as e:
+    #     gui.BasicUI.HandleError(e)
+
+    # return True
+
+    @staticmethod
     def setSkipEmptyVerses(skipEmptyVerses):
-        # First delete the old output path
-        db.DATABASE.execute("DELETE FROM settings WHERE name = 'skip_empty_verses';")
-        # Then insert the new output path
-        db.DATABASE.execute("INSERT INTO settings (name, value) VALUES ('skip_empty_verses', %s);", (skipEmptyVerses,))
-        logging.info(f"Skip empty verses updated to {skipEmptyVerses} in database.")
+        global SkipVerses
+        SkipVerses = skipEmptyVerses
+        logging.info(
+            f"Skip empty verses updated to {skipEmptyVerses}.")
 
+    @staticmethod
     def getSkipEmptyVerses():
-        try:
-            skipEmptyVerses = db.DATABASE.fetch_one("SELECT value FROM settings where name = 'skip_empty_verses';")
+        global SkipVerses
+        return SkipVerses
 
-            if skipEmptyVerses is None:
-                return True
-
-            # Parse the result
-            skipEmptyVerses = skipEmptyVerses[0]
-
-            skipEmptyVerses = skipEmptyVerses.capitalize()
-
-            if skipEmptyVerses == "True":
-                return True
-            elif skipEmptyVerses == "False":
-                return False
-            else:
-                raise Exception(f"Invalid value for skip_empty_verses: {skipEmptyVerses}")    
-        except Exception as e:
-            gui.BasicUI.HandleError(e)    
-
-        return True
 
 def configure_logging():
 
